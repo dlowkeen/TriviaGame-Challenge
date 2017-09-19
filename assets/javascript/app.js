7429654256e5c733853acf148383bee7f8a06244
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2017-09-18 21:43:11 -0700

    Added Timer and Counters

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Variables
 var produce = [
 	{	name: "Avocados",
 		image: "<img src='assets/images/avocados.jpg'>",
@@ -26,109 +27,109 @@ var produce = [
 	{	name: "Cantaloupe",
 		image: "<img src='assets/images/cantaloupe.png'>",
 		answerOptions: ["4", "69", "12", "36"],
-		hint: "hint5",
+		hint: "Cantaloupe Sex",
 		answer: "69"
 		},
 	{	name: "Beefsteak Tomatoes",
 		image: "<img src='assets/images/beefsteak-tomato.jpg'>",
 		answerOptions: ["5", "31", "73", "77"],
-		hint: "hint4",
+		hint: "'The Year Dark Side of The Moon' was released",
 		answer: "73"
 		},
 	{	name: "Strawberries",
 		image: "<img src='assets/images/Strawberry.jpg'>",
 		answerOptions: ["2", "1", "3", "4"],
-		hint: "hint4",
+		hint: "How many 'e's?",
 		answer: "2"
 		},
 	{	name: "Seedless Cucumbers",
 		image: "<img src='assets/images/seedlesscucumber.jpg'>",
 		answerOptions: ["4", "87", "78", "90"],
-		hint: "hint4",
+		hint: "The year Robocop was released",
 		answer: "87"
 		},
 	{	name: "Bananas",
 		image: "<img src='assets/images/bananas.jpg'>",
 		answerOptions: ["2", "4", "3", "7"],
-		hint: "hint4",
+		hint: "1, 2, __",
 		answer: "3"
 		},
 	{	name: "Cucumbers",
 		image: "<img src='assets/images/cucumbers.jpg'>",
-		answerOptions: ["9", "44", "4", "8"],
-		hint: "hint4",
+		answerOptions: ["9", "44", "4", "7"],
+		hint: "Cucmbers __ everyone!",
 		answer: "4"
 		},
 	{	name: "Limes",
 		image: "<img src='assets/images/limes.jpg'>",
 		answerOptions: ["8", "49", "21", "9"],
-		hint: "hint4",
+		hint: "Kobe's first jersey",
 		answer: "8"
 		},
 	{	name: "Mangoes",
 		image: "<img src='assets/images/mango.jpg'>",
 		answerOptions: ["17", "89", "9", "5"],
-		hint: "hint4",
+		hint: "lower case g's look like a __",
 		answer: "9"
 		},
 	{	name: "Green Peppers",
 		image: "<img src='assets/images/green-pepper.jpg'>",
 		answerOptions: ["10", "12", "11", "51"],
-		hint: "hint4",
+		hint: "How many fingers do you have?",
 		answer: "10"
 		},
 	{	name: "Iceberg Lettuce",
 		image: "<img src='assets/images/iceberg-lettuce.jpg'>",
 		answerOptions: ["12", "31", "13", "33"],
-		hint: "hint4",
+		hint: "Unlucky number",
 		answer: "13"
 		},
 	{	name: "Pineapples",
 		image: "<img src='assets/images/pineapple.jpg'>",
 		answerOptions: ["18", "81", "11", "88"],
-		hint: "hint4",
+		hint: "Coming of age",
 		answer: "18"
 		},
 	{	name: "Organic Bananas",
 		image: "<img src='assets/images/organic-bananas.jpg'>",
 		answerOptions: ["33", "41", "116", "22"],
-		hint: "hint4",
+		hint: "Taylor Swift is feeling __",
 		answer: "22"
 		},
 	{	name: "Zucchini",
 		image: "<img src='assets/images/zucchini.jpg'>",
 		answerOptions: ["143", "101", "127", "107"],
-		hint: "hint4",
+		hint: "James Franco ___ Hours",
 		answer: "127"
 		},
 	{	name: "Tomatillos",
 		image: "<img src='assets/images/tomatillos.jpg'>",
 		answerOptions: ["72", "96", "27", "22"],
-		hint: "hint4",
+		hint: "Cut the T in half, count the l's",
 		answer: "72"
 		},
 	{	name: "Vine Tomatoes",
 		image: "<img src='assets/images/vine-tomatoes.jpg'>",
 		answerOptions: ["5", "6", "7", "16"],
-		hint: "hint4",
+		hint: "# of letters divided by 2",
 		answer: "6"
 		},
 	{	name: "Honeydew Melons",
 		image: "<img src='assets/images/honeydew.jpg'>",
 		answerOptions: ["18", "8", "9", "88"],
-		hint: "hint4",
+		hint: "o+o",
 		answer: "88"
 		},
 	{	name: "Grape Fruit",
 		image: "<img src='assets/images/grapefruit.jpg'>",
 		answerOptions: ["142", "138", "134", "101"],
-		hint: "hint4",
+		hint: "Freeway to Pasadena",
 		answer: "134"
 		},
 	{	name: "Yellow Squash",
 		image: "<img src='assets/images/yellow-squash.jpg'>",
-		answerOptions: ["130", "167", "14", "130"],
-		hint: "hint4",
+		answerOptions: ["131", "167", "14", "130"],
+		hint: "One Flirty",
 		answer: "130"
 		}
 ];
@@ -136,17 +137,10 @@ var produce = [
 var correct = 0;
 var incorrect = 0;
 var questionCounter = 0;
+var clockRunning = false;
+var timer = 15;
+var intervalId;
 	
-
-	$("#main").hide();
-	$("#hint-display").hide();
-
-// Hide "Play" button when clicked
-	$("#play").on("click", function(event){
-		$(this).hide();
-		$("#main").show();
-		displayHTML();
-	});	
 
 // Displays questions, answers, hints in main section
 	function displayHTML() {
@@ -160,31 +154,90 @@ var questionCounter = 0;
 
 	}
 
-	function finalPage() {
-		$("#main").html("Finished");
+	function run() {
+		intervalId = setInterval(decrement, 1000);
 	}
+
+	function decrement() {
+		if (timer > 0) {
+			timer--;
+		}
+		if (timer === 0) {
+			timeOutLoss();
+			timer = 15;
+		}	
+		$("#timer").html("Seconds Remaining: " + timer);
+	}
+
+	function stop() {
+		clearInterval(intervalId);
+	}
+
+	function win(){
+		console.log("You Win!");
+		correct++;	
+		wait();
+		$("#hint-display").hide();
+	}
+
+	function lose(){
+		console.log("You Lose!");
+		incorrect++;
+		wait();
+		$("#hint-display").hide();		
+	}
+
+	function timeOutLoss(){
+		console.log("You Timed Out!");
+		incorrect++;
+		wait();
+		$("#hint-display").hide();
+	}
+
+	function wait() {
+	if (questionCounter < produce.length) {
+		questionCounter++;
+		displayHTML();
+		timer = 15;
+		run();
+	}
+	else (questionCounter === produce.length) 
+		finalPage();
+	}
+
+// At the end of the game, total up: correct, incorrect and give a grade
+	function finalPage() {
+		$("#timer").html("Grade: " + correct / (correct+incorrect) *100 + "%");
+		stop();
+	}
+
+
+// Begin game
+	$("#main").hide();
+	$("#hint-display").hide();
+
+// Hide "Play" button when clicked
+	$("#play").on("click", function(event){
+		$(this).hide();
+		$("#main").show();
+		displayHTML();
+		run();
+	});	
 
 // When answer is clicked, move on to next question
 $(".answer").on("click", function(event){
-
+	
 	var selectedAnswer = $(this).text();
 
 		if (selectedAnswer === produce[questionCounter].answer) {
-			console.log("You Win!");
-			questionCounter++;
-			correct++;
-			displayHTML();
-			$("#hint-display").hide();
+			win();
+			timer = 15;
 		}
 
 		else {
-			console.log("You Lose!");
-			questionCounter++;
-			incorrect++;
-			displayHTML();
-			$("#hint-display").hide();
-	}
-	console.log(questionCounter);
+			lose();
+			timer = 15;
+		}
 
 });
 
@@ -192,122 +245,3 @@ $(".answer").on("click", function(event){
 	$("#hint").on("click", function(event){
 		$("#hint-display").show();
 	})
-
-
-// At the end of the game, total up: correct, incorrect and give a grade
-
-	if (questionCounter === produce.length) {
-			console.log("Final");
-	}
-
-
-
-
-// function finalScreen() {
-// 	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
-// 	$(".mainArea").html(gameHTML);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
-
-// $("body").on("click", ".start-button", function(event){
-// 	event.preventDefault();  // added line to test issue on GitHub Viewer
-// 	clickSound.play();
-// 	generateHTML();
-
-// 	timerWrapper();
-
-// }); // Closes start-button click
-
-// $("body").on("click", ".answer", function(event){
-// 	//answeredQuestion = true;
-// 	clickSound.play();
-// 	selectedAnswer = $(this).text();
-// 	if(selectedAnswer === correctAnswers[questionCounter]) {
-// 		//alert("correct");
-
-// 		clearInterval(theClock);
-// 		generateWin();
-// 	}
-// 	else {
-// 		//alert("wrong answer!");
-// 		clearInterval(theClock);
-// 		generateLoss();
-// 	}
-// }); // Close .answer click
-
-// $("body").on("click", ".reset-button", function(event){
-// 	clickSound.play();
-// 	resetGame();
-// }); // Closes reset-button click
-
-// });  //  Closes jQuery wrapper
-
-// function generateLossDueToTimeOut() {
-// 	unansweredTally++;
-// 	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-// 	$(".mainArea").html(gameHTML);
-// 	setTimeout(wait, 4000);  //  change to 4000 or other amount
-// }
-
-
-// function wait() {
-// 	if (questionCounter < 7) {
-// 	questionCounter++;
-// 	generateHTML();
-// 	counter = 30;
-// 	timerWrapper();
-// 	}
-// 	else {
-// 		finalScreen();
-// 	}
-// }
-
-// function timerWrapper() {
-// 	theClock = setInterval(thirtySeconds, 1000);
-// 	function thirtySeconds() {
-// 		if (counter === 0) {
-// 			clearInterval(theClock);
-// 			generateLossDueToTimeOut();
-// 		}
-// 		if (counter > 0) {
-// 			counter--;
-// 		}
-// 		$(".timer").html(counter);
-// 	}
-// }
-
-// function finalScreen() {
-// 	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
-// 	$(".mainArea").html(gameHTML);
-// }
-
-// function resetGame() {
-// 	questionCounter = 0;
-// 	correctTally = 0;
-// 	incorrectTally = 0;
-// 	unansweredTally = 0;
-// 	counter = 30;
-// 	generateHTML();
-// 	timerWrapper();
-// }
-
-
-// var theClock;
-// var unansweredTally = 0;
